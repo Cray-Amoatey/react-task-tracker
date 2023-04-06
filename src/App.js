84450ab77bddeb0d085d7c80f61a7c5a42adeaf7
Cray-Amoatey <amoatey.cray@gmail.com>
2023-04-06 15:47:47 +0000

    no tasks to show

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : ''}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks To Show'}
     </div>
   );
 };
