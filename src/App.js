7f8d1bcbefa65f7083f005631b41191ee4d24a76
Cray-Amoatey <amoatey.cray@gmail.com>
2023-04-06 16:03:04 +0000

    onToggle effect when double click

--- src/App.js
+++ src/App.js
@@ -27,12 +27,21 @@ const App = () => {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id ))
   };
+
+   // Toggle Reminder
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
+
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks To Show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle = {toggleReminder} /> : 'No Tasks To Show'}
     </div>
-  );
+  )
+
+ 
 };
 
 export default App;
