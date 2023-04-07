51a92c480bb52a4822ed450b353f7cb6ca75fd3b
Cray-Amoatey <amoatey.cray@gmail.com>
2023-04-07 09:22:55 +0000

    submit function within app.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,13 @@ const App = () => {
       reminder: false,
     },
   ]);
+
+  // Add Task
+  const addTask = (task) => {
+    console.log(task)
+  }
+
+
   //  Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -41,7 +48,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <AddTask/>
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
